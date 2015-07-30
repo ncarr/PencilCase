3b176db54f7d66ea6858666bd182aac20a2c1306
ncarr <nicholas.carr@rogers.com>
2015-07-30 12:47:43 -0400

    Keep updated copy of what I've been working on
    PMs and refreshing, essentially

--- editpm.php
+++ editpm.php
@@ -1,31 +1,31 @@
 <?php
-    if ($_GET["id"]) {
+    if ($_POST["id"]) {
         session_start();
         $db = include("sqlconnect.php");
         $mysqli = $db->manual();
         $stmt = $mysqli->prepare("SELECT sender, receiver FROM pms WHERE id = ?");
         $stmt->bind_param('i', $id);
-        $id = $_GET["id"];
+        $id = $_POST["id"];
         $stmt->execute();
         $stmt->bind_result($sender, $receiver);
         $stmt->store_result();
         if ($stmt->num_rows == 1)
             $stmt->fetch();
         else {
-            header("Location: pm.php?delexistenceerr");
+            header("Location: pm.php?editexistenceerr");
             exit();
         }
         $stmt->close();
         $mysqli->close();
         if ($_SESSION["uid"] != $sender && $_SESSION["uid"] != "INSERT ENGINEER ID HERE") {
-            header("Location: pm.php?delautherr");
+            header("Location: pm.php?editautherr");
             exit();
         }
-        $id = $_GET["id"];
-        $vals = array('i', &$id);
-        $db->prep("DELETE FROM pms WHERE id = ?", $vals);
+        $id = $_POST["id"];
+        $content = strip_tags(preg_replace("/<\/p>|<p>/", '', preg_replace("/\r\n|\r|\n|<\/p><p>/", '<br>', $_POST["content"])), "<p><br><br/><a>");
+        $vals = array('si', &$content, &$id);
+        $db->prep("UPDATE pms SET content=? WHERE id=?", $vals);
         $vals2 = array('s', &$receiver);
         $db->prep("INSERT INTO pmu (receiver, timestamp) VALUES (?, now())", $vals2);
-        header("Location: " . $_GET["return"]);
     }
 ?>
